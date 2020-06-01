c844c72b09fc13033f6b4538e942fde7c0ec7328
vinitra <vinitras@gmail.com>
2020-06-01 12:34:30 -0700

    remove lfs config, update to git lfs pull to only address recent changes, test_models doesn't test lfs pointer files

--- .azure-pipelines/test_models.py
+++ .azure-pipelines/test_models.py
@@ -1,15 +1,16 @@
 import onnx
-import numpy as np
 from pathlib import Path
 
 # identify list of onnx models in model Zoo
 model_list = []
 
 for path in Path('text').rglob('*.onnx'):
-    model_list.append(str(path))
+    if path.stat().st_size >= 200:
+        model_list.append(str(path))
 
 for path in Path('vision').rglob('*.onnx'):
-    model_list.append(str(path))
+    if path.stat().st_size >= 200:
+        model_list.append(str(path))
 
 # run checker on each model
 for model_name in model_list:
@@ -17,3 +18,5 @@ for model_name in model_list:
     onnx.checker.check_model(model)
 
     print("Model ", model_name, "has been successfully checked!")
+
+print(len(model_list), " models checked.")
