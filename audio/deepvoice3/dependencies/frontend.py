0c150696e113bc1eb461c5e7102b189412479e94
kundanapillari <kundanapjs@gmail.com>
2020-06-26 15:21:32 -0700

    Updated frontend.py

--- audio/deepvoice3/dependencies/frontend.py
+++ audio/deepvoice3/dependencies/frontend.py
@@ -1,3 +1,8 @@
+'''
+DISCLAIMER 
+This file was originally created by the developers of the DeepVoice3 repository and is used to preprocess input data.
+'''
+
 # coding: utf-8
 from text.symbols import symbols
 
@@ -8,7 +13,7 @@ n_vocab = len(symbols)
 
 _arpabet = nltk.corpus.cmudict.dict()
 
-
+# Assigning pronounciation to the text 
 def _maybe_get_arpabet(word, p):
     try:
         phonemes = _arpabet[word][0]
@@ -18,12 +23,12 @@ def _maybe_get_arpabet(word, p):
 
     return '{%s}' % phonemes if random() < p else word
 
-
+# Splits the words in the input text 
 def mix_pronunciation(text, p):
     text = ' '.join(_maybe_get_arpabet(word, p) for word in text.split(' '))
     return text
 
-
+# Decodes the input text into valid input features for unique pronunciation
 def text_to_sequence(text, p=0.0):
     if p >= 0:
         text = mix_pronunciation(text, p)
