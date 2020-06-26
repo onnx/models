a64bbfcbc37dc8fa29101e63bbb6f64a31efbbc8
kundanapillari <kundanapjs@gmail.com>
2020-06-26 15:33:32 -0700

    Changed symbols.py

--- audio/deepvoice3/dependencies/text/symbols.py
+++ audio/deepvoice3/dependencies/text/symbols.py
@@ -1,4 +1,10 @@
 '''
+Defines the set of symbols used in text input to the model.
+The default is a set of ASCII characters that works well for English or text that has been run
+through Unidecode. For other data, you can modify _characters. 
+'''
+
+'''
 DISCLAIMER 
 This file was originally created by the developers of the DeepVoice3 repository and is used to preprocess input data.
 '''
