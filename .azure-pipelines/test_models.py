d1be6c8471ada2f6e898a5c552a39cf02a98d0ea
vinitra <vinitras@gmail.com>
2020-07-16 14:37:44 -0700

    fixing commit logic

--- .azure-pipelines/test_models.py
+++ .azure-pipelines/test_models.py
@@ -10,10 +10,16 @@ def save_file(url, file_name):
 
 PIPE = subprocess.PIPE
 cwd_path = Path.cwd()
-obtain_diff = subprocess.Popen(['git', 'diff', '--name-only', '--diff-filter=AM', 'origin/master', 'HEAD'], cwd=cwd_path, stdout=PIPE, stderr=PIPE)
 
+# obtain list of added or modified files in this PR
+obtain_diff = subprocess.Popen(['git', 'diff', '--name-only', '--diff-filter=AM', 'origin/master', 'HEAD'], cwd=cwd_path, stdout=PIPE, stderr=PIPE)
 stdoutput, stderroutput = obtain_diff.communicate()
 diff_list = stdoutput.split()
+
+# obtain git commit ID to extract git lfs models accordingly
+obtain_commit_id = subprocess.Popen(['git', 'rev-parse', 'HEAD'], stdout=PIPE, stderr=PIPE)
+stdoutput, stderroutput = obtain_commit_id.communicate()
+commit_id = str(stdoutput).replace("b'","").replace("'", "")[:-2]
 
 # identify list of changed onnx models in model Zoo
 model_list = [str(model).replace("b'","").replace("'", "") for model in diff_list if ".onnx" in str(model)]
@@ -23,7 +29,7 @@ for model_path in model_list:
     model_name = model_path.split('/')[-1]
     print("Testing:", model_name)
 
-    model_url = "https://github.com/onnx/models/raw/master/" + model_path
+    model_url = "https://github.com/onnx/models/raw/" + commit_id + "/" + model_path
     save_file(model_url, model_path)
 
     model = onnx.load(model_path)
