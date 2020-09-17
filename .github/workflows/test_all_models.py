2b40bf87d927df5507c056864590a908e8384024
Chun-Wei Chen <jacky82226@gmail.com>
2020-09-17 14:09:14 -0700

    use git-lfs cache to avoid redownloading
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- .github/workflows/test_all_models.py
+++ .github/workflows/test_all_models.py
@@ -13,11 +13,12 @@ TEST_DIR = 'test_dir'
 
 def run_lfs_install():
     result = subprocess.run(['git', 'lfs', 'install'], cwd=cwd_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print("Git LFS install completed with return code=" + str(result.returncode))
+    print('Git LFS install completed with return code= {}'.format(result.returncode))
+
 
 def pull_lfs_file(file_name):
-    result = subprocess.run(['git', 'lfs', 'pull', '--include', file_name, '--exclude', '\"\"'], cwd=cwd_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print("LFS pull completed with return code=" + str(result.returncode))
+    result = subprocess.run(['git', 'lfs', 'pull', '--include', file_name, '--exclude', '\'\''], cwd=cwd_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print('LFS pull completed with return code= {}'.format(result.returncode))
 
 
 def save_file(url, file_name):
@@ -25,23 +26,20 @@ def save_file(url, file_name):
   with open(file_name, 'wb') as f:
     f.write(r.content)
 
-def check_by_onnx(model_path):
+def check_by_onnx(model_path, model_name):
     model = onnx.load(model_path)
     onnx.checker.check_model(model)
+    print('[PASS]: {} is checked by onnx. '.format(model_name))
 
-def check_by_onnxruntime(model_path):
+def check_by_onnxruntime(model_path, model_name):
     onnxruntime.InferenceSession(model_path)
     ort_test_dir_utils.create_test_dir(model_path, './', TEST_DIR)
-    ort_test_dir_utils.run_test_dir(test_dir)
+    ort_test_dir_utils.run_test_dir(TEST_DIR)
+    print('[PASS]: {} is checked by onnxruntime. '.format(model_name))
 
 
 def main():
-    # obtain list of added or modified files in this PR
-    obtain_diff = subprocess.Popen(['git', 'diff', '--name-only', '--diff-filter=AM', 'origin/master', 'HEAD'],
-    cwd=cwd_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdoutput, stderroutput = obtain_diff.communicate()
-    diff_list = stdoutput.split()
-    model_directory = ["."] # ['text', 'vision']
+    model_directory = ['vision/classification/efficientnet-lite4/'] #['text', vision'] # ['vision/classification/efficientnet-lite4/']
     model_list = []
 
     for directory in model_directory:
@@ -55,31 +53,30 @@ def main():
     # run lfs install before starting the tests
     run_lfs_install()
 
-    print("\n=== Running ONNX Checker on added models ===\n")
+    print('\n=== Running ONNX Checker on added models ===\n')
     # run checker on each model
     failed_models = []
     for model_path in model_list:
         model_name = model_path.split('/')[-1]
-        print("Testing:", model_name)
+        print('----------------Testing: {}----------------'.format(model_name))
 
         try:
             pull_lfs_file(model_path)
-            check_by_onnx(model_path)
-            check_by_onnxruntime(model_path)
-            print("Model", model_name, "has been successfully checked!")
+            check_by_onnx(model_path, model_name)
+            check_by_onnxruntime(model_path, model_name)
+            if os.path.exists(TEST_DIR) and os.path.isdir(TEST_DIR):
+                shutil.rmtree(TEST_DIR)
+
         except Exception as e:
-            print(e)
+            print('[FAIL]: {}'.format(e))
             failed_models.append(model_path)
 
-        if TEST_DIR.exists() and TEST_DIR.is_dir():
-            shutil.rmtree(TEST_DIR)
 
-    if len(failed_models) != 0:
-        print(str(len(failed_models)) +" models failed onnx checker.")
+    if len(failed_models) == 0:
+        print('{} models have been checked.'.format(len(model_list)))
+    else:
+        print('In all {} models, {} models failed.'.format(len(model_list), len(failed_models)))
         sys.exit(1)
-
-    print(len(model_list), "model(s) checked.")
-
       
 
 if __name__ == '__main__':
