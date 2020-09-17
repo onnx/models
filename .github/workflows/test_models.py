545d83c570636d4f8ee849a559cc3084cd50fb7c
Chun-Wei Chen <jacky82226@gmail.com>
2020-09-17 11:58:08 -0700

    add pull files from lfs
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- .github/workflows/test_models.py
+++ .github/workflows/test_models.py
@@ -4,6 +4,17 @@ import subprocess
 import requests
 import ort_test_dir_utils
 import onnxruntime
+import sys
+
+cwd_path = Path.cwd()
+
+def run_lfs_install():
+    result = subprocess.run(['git', 'lfs', 'install'], cwd=cwd_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("Git LFS install completed with return code=" + str(result.returncode))
+
+def pull_lfs_file(file_name):
+    result = subprocess.run(['git', 'lfs', 'pull', '--include', file_name, '--exclude', '\"\"'], cwd=cwd_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("LFS pull completed with return code=" + str(result.returncode))
 
 
 def save_file(url, file_name):
@@ -23,38 +34,40 @@ def check_by_onnxruntime(model_path):
 
 
 def main():
-    PIPE = subprocess.PIPE
-    cwd_path = Path.cwd()
-
     # obtain list of added or modified files in this PR
-    obtain_diff = subprocess.Popen(['git', 'diff', '--name-only', '--diff-filter=AM', 'origin/master', 'HEAD'], cwd=cwd_path, stdout=PIPE, stderr=PIPE)
+    obtain_diff = subprocess.Popen(['git', 'diff', '--name-only', '--diff-filter=AM', 'origin/master', 'HEAD'],
+    cwd=cwd_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdoutput, stderroutput = obtain_diff.communicate()
     diff_list = stdoutput.split()
-
-    # obtain git commit ID to extract git lfs models accordingly
-    obtain_commit_id = subprocess.Popen(['git', 'rev-parse', 'HEAD'], stdout=PIPE, stderr=PIPE)
-    stdoutput, stderroutput = obtain_commit_id.communicate()
-    commit_id = str(stdoutput).replace("b'","").replace("'", "")[:-2]
 
     # identify list of changed onnx models in model Zoo
     model_list = [str(model).replace("b'","").replace("'", "") for model in diff_list if ".onnx" in str(model)]
 
+    # run lfs install before starting the tests
+    run_lfs_install()
+
     print("\n=== Running ONNX Checker on added models ===\n")
     # run checker on each model
+    failed_models = []
     for model_path in model_list:
         model_name = model_path.split('/')[-1]
         print("Testing:", model_name)
 
-        model_url = "https://github.com/onnx/models/raw/" + commit_id + "/" + model_path
-        save_file(model_url, model_path)
+        try:
+            pull_lfs_file(model_path)
+            check_by_onnx(model_path)
+            check_by_onnxruntime(model_path)
+            print("Model", model_name, "has been successfully checked!")
+        except Exception as e:
+            print(e)
+            failed_models.append(model_path)
 
-        check_by_onnx(model_path)
-        check_by_onnxruntime(model_path)
-
-
-        print("Model", model_name, "has been successfully checked!")
+    if len(failed_models) != 0:
+        print(str(len(failed_models)) +" models failed onnx checker.")
+        sys.exit(1)
 
     print(len(model_list), "model(s) checked.")
+
       
 
 if __name__ == '__main__':
