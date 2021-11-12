e923e0e19195f7d3ab3ef833d2488e476a6ecbf3
Mark Hamilton <marhamil@microsoft.com>
2021-11-12 13:08:11 -0500

    Update manifest and add feature tensor information (#478)
    Signed-off-by: mhamilton723 <marhamil@microsoft.com>

--- workflow_scripts/generate_onnx_hub_manifest.py
+++ workflow_scripts/generate_onnx_hub_manifest.py
@@ -9,6 +9,8 @@ import typepy
 from os.path import join, split
 import onnxruntime as ort
 from onnxruntime.capi.onnxruntime_pybind11_state import NotImplemented
+import onnx
+from onnx import shape_inference
 
 
 # Acknowledgments to pytablereader codebase for this function
@@ -112,23 +114,40 @@ def get_model_tags(row):
     return [tag.replace("_", " ") for tag in raw_tags]
 
 
-def get_model_io_ports(source_file):
+def get_model_ports(source_file, metadata, model_name):
     model_path = join("..", source_file)
     try:
         # Hide graph warnings. Severity 3 means error and above.
         ort.set_default_logger_severity(3)
-        # Start from ORT 1.10, ORT requires explicitly setting the providers parameter if you want to use execution providers
-        # other than the default CPU provider (as opposed to the previous behavior of providers getting set/registered by default
+        # Start from ORT 1.10, ORT requires explicitly setting the providers parameter
+        # if you want to use execution providers
+        # other than the default CPU provider (as opposed to the previous behavior of
+        # providers getting set/registered by default
         # based on the build flags) when instantiating InferenceSession.
         # For example, if NVIDIA GPU is available and ORT Python package is built with CUDA, then call API as following:
         # ort.InferenceSession(path/to/model, providers=['CUDAExecutionProvider'])
         session = ort.InferenceSession(model_path)
         inputs = session.get_inputs()
         outputs = session.get_outputs()
-        return {
+        io_ports = {
             "inputs": [{"name": input.name, "shape": input.shape, "type": input.type} for input in inputs],
             "outputs": [{"name": output.name, "shape": output.shape, "type": output.type} for output in outputs],
         }
+
+        extra_ports = None
+        if "classification" in metadata["tags"]:
+            inferred_model = shape_inference.infer_shapes(onnx.load(model_path))
+            nodes = list(inferred_model.graph.value_info)
+            if model_name in feature_tensor_names:
+                node_name = feature_tensor_names[model_name]
+                node = [n for n in nodes if n.name == node_name][0]
+                shape = [d.dim_value for d in list(node.type.tensor_type.shape.dim)]
+                extra_ports = {"features": [
+                    {"name": node.name, "shape": shape}
+                ]}
+
+        return io_ports, extra_ports
+
     except NotImplemented:
         print(
             'Failed to load model from {}. Run `git lfs pull --include="{}" --exclude=""` '
@@ -136,36 +155,76 @@ def get_model_io_ports(source_file):
                 model_path, source_file
             )
         )
-        return None
+        return None, None
 
+
+feature_tensor_names = {
+    'AlexNet': 'fc7_1',
+    'CaffeNet': 'fc7_1',
+    'DenseNet-121': 'pool5_1',
+    'EfficientNet-Lite4': 'efficientnet-lite4/model/head/AvgPool:0',
+    'GoogleNet': 'pool5/7x7_s1_2',
+    'Inception-1': 'pool5/7x7_s1_2',
+    'Inception-2': 'pool5/7x7_s1_1',
+    'MobileNet v2-1.0': '464',
+    'R-CNN ILSVRC13': 'fc7_1',
+    'ResNet18': 'resnetv15_pool1_fwd',
+    'ResNet34': 'resnetv16_pool1_fwd',
+    'ResNet50': 'resnetv17_pool1_fwd',
+    'ResNet101': 'resnetv18_pool1_fwd',
+    'ResNet152': 'resnetv19_pool1_fwd',
+    'ResNet50_fp32': 'resnetv17_pool1_fwd',
+    'ResNet50_int8': 'flatten_473_quantized',
+    'ResNet50-caffe2': 'gpu_0/pool5_1',
+    'ResNet18-v2': 'resnetv22_pool1_fwd',
+    'ResNet34-v2': 'resnetv23_pool1_fwd',
+    'ResNet50-v2': 'resnetv24_pool1_fwd',
+    'ResNet101-v2': 'resnetv25_pool1_fwd',
+    'ResNet152-v2': 'resnetv27_pool1_fwd',
+    'ShuffleNet-v1': 'gpu_0/final_avg_1',
+    'ShuffleNet-v2': '611',
+    'ShuffleNet-v2-fp32': '611',
+    'ShuffleNet-v2-int8': '611',
+    'SqueezeNet 1.1': 'squeezenet0_pool3_fwd',
+    'SqueezeNet 1.0': 'pool10_1',
+    'VGG 16': "flatten_70",
+    'VGG 16-bn': "flatten_135",
+    'VGG 19': "flatten_82",
+    'VGG 19-bn': "flatten_162",
+    'VGG 16-int8': "flatten_70_quantized",
+    'VGG 19-caffe2': "fc7_3",
+    'ZFNet-512': 'gpu_0/fc7_2'
+}
 
 output = []
 for i, row in renamed.iterrows():
+    if len(row["model"].contents) > 0 and len(row["model_path"].contents) > 0:
+        model_name = row["model"].contents[0]
+        model_info = get_file_info(row, "model_path")
+        model_path = model_info.pop("model_path")
+        metadata = model_info
+        metadata["tags"] = get_model_tags(row)
+        io_ports, extra_ports = get_model_ports(model_path, metadata, model_name)
+        if io_ports is not None:
+            metadata["io_ports"] = io_ports
+        if extra_ports is not None:
+            metadata["extra_ports"] = extra_ports
 
-    model_info = get_file_info(row, "model_path")
-    model_path = model_info.pop("model_path")
-    metadata = model_info
-    metadata["tags"] = get_model_tags(row)
-    io_ports = get_model_io_ports(model_path)
-    if io_ports is not None:
-        metadata["io_ports"] = io_ports
+        try:
+            for k, v in get_file_info(row, "model_with_data_path").items():
+                metadata[k] = v
+        except (AttributeError, FileNotFoundError) as e:
+            print("no model_with_data in file {}".format(row["source_file"]))
 
-    try:
-        for k, v in get_file_info(row, "model_with_data_path").items():
-            metadata[k] = v
-    except AttributeError as e:
-        print("no model_with_data in file {}".format(row["source_file"]))
+        try:
+            opset = int(row["opset_version"].contents[0])
+        except ValueError:
+            print("malformed opset {} in {}".format(row["opset_version"].contents[0], row["source_file"]))
+            continue
 
-    try:
-        opset = int(row["opset_version"].contents[0])
-    except ValueError:
-        print("malformed opset {} in {}".format(row["opset_version"].contents[0], row["source_file"]))
-        continue
-
-    if len(row["model"].contents) > 0:
         output.append(
             {
-                "model": row["model"].contents[0],
+                "model": model_name,
                 "model_path": model_path,
                 "onnx_version": row["onnx_version"].contents[0],
                 "opset_version": int(row["opset_version"].contents[0]),
