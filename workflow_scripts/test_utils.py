cf382db7781fc8193249386d6b50a4753659d058
Chun-Wei Chen <jacky82226@gmail.com>
2022-06-22 10:45:50 -0700

    Update broken test_data_set by the latest ORT (#534)
    * update ort utilities
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * fix broken input/outputs .tar.gz
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * update with dim_size 1 for symbolic shapes
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * Update resnet50-v1-12
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * enable work_scripts to create_if_failed
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * remove checked models and dirs
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * enable test_models to create if failed
    
    Signed-off-by: jcwchen <jacky82226@gmail.com>
    
    * nit grammar
    
    Signed-off-by: jcwchen <jacky82226@gmail.com>
    
    * use keep for flexibility
    
    Signed-off-by: jcwchen <jacky82226@gmail.com>
    
    * remove remove_dir and fix by a few reviews
    
    Signed-off-by: jcwchen <jacky82226@gmail.com>
    
    * remove single quote bug
    
    Signed-off-by: jcwchen <jacky82226@gmail.com>
    
    * use model name instead
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * revert new .tar.gz with wrong dir name
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * revert new .tar.gz with wrong dir name
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * use model_name as the directory name
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * update resnet-12
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * fix all broken test_data_set
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>
    
    * use osp.basename and splitext
    
    Signed-off-by: Chun-Wei Chen <jacky82226@gmail.com>

--- workflow_scripts/test_utils.py
+++ workflow_scripts/test_utils.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import subprocess
 import tarfile
 import os
-import shutil
+from shutil import rmtree
 
 TEST_ORT_DIR = 'ci_test_dir'
 TEST_TAR_DIR = 'ci_test_tar_dir'
@@ -23,6 +23,11 @@ def run_lfs_install():
 def pull_lfs_file(file_name):
     result = subprocess.run(['git', 'lfs', 'pull', '--include', file_name, '--exclude', '\'\''], cwd=cwd_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print('LFS pull completed with return code= {}'.format(result.returncode))
+
+
+def run_lfs_prune():
+    result = subprocess.run(['git', 'lfs', 'prune'], cwd=cwd_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(f'LFS prune completed with return code= {result.returncode}')
 
 
 def extract_test_data(file_path):
@@ -51,9 +56,9 @@ def get_model_and_test_data(directory_path):
 
 def remove_tar_dir():
     if os.path.exists(TEST_TAR_DIR) and os.path.isdir(TEST_TAR_DIR):
-        shutil.rmtree(TEST_TAR_DIR)
+        rmtree(TEST_TAR_DIR)
 
 
 def remove_onnxruntime_test_dir():
     if os.path.exists(TEST_ORT_DIR) and os.path.isdir(TEST_ORT_DIR):
-        shutil.rmtree(TEST_ORT_DIR)
+        rmtree(TEST_ORT_DIR)
