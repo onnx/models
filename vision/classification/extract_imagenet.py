36d3c572cff2cc16e5f4352d64f261e5aa181fc6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 00:06:10 +0000

    Adding numeric_owner as keyword arguement

--- vision/classification/extract_imagenet.py
+++ vision/classification/extract_imagenet.py
@@ -63,7 +63,7 @@ def extract_train(tar_fname, target_dir):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(f, class_dir)
@@ -91,7 +91,7 @@ def extract_val(tar_fname, target_dir):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, target_dir)
