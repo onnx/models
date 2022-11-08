11f2e3cd75b366befe54d560a1738f5b7cc72e16
jcwchen <jacky82226@gmail.com>
2022-11-08 11:31:23 -0800

    detect vnni
    Signed-off-by: jcwchen <jacky82226@gmail.com>

--- workflow_scripts/check_model.py
+++ workflow_scripts/check_model.py
@@ -10,8 +10,8 @@ import tarfile
 import test_utils
 
 
-def has_avx512f_support():
-    return "avx512f" in set(get_cpu_info()["flags"])
+def has_vnni_support():
+    return "avx512vnni" in set(get_cpu_info()["flags"])
 
 
 def run_onnx_checker(model_path):
@@ -20,8 +20,8 @@ def run_onnx_checker(model_path):
 
 
 def ort_skip_reason(model_path):
-    if ("-int8" in model_path or "-qdq" in model_path) and not has_avx512f_support():
-        return f"Skip ORT test for {model_path} because this machine lacks avx512f support and the output.pb was produced with avx512f support."
+    if ("-int8" in model_path or "-qdq" in model_path) and not has_vnni_support():
+        return f"Skip ORT test for {model_path} because this machine lacks avx512vnni support and the output.pb was produced with avx512vnni support."
     model = onnx.load(model_path)
     if model.opset_import[0].version < 7:
         return f"Skip ORT test for {model_path} because ORT only supports opset version >= 7"
