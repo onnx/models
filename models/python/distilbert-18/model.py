943fc343b3629a28e34c6d6d8244bf6055257912
jcwchen <jacky82226@gmail.com>
2023-06-07 15:59:40 -0700

    add bart.py
    Signed-off-by: jcwchen <jacky82226@gmail.com>

--- models/python/distilbert-18/model.py
+++
@@ -1,4 +0,0 @@
-import torch
-from transformers import AutoModelForSequenceClassification
-model = AutoModelForSequenceClassification.from_pretrained("textattack/distilbert-base-cased-CoLA")
-torch.onnx.export(model.base_model, model.dummy_inputs, "distilbert-18.onnx", verbose=True, input_names=["input_ids", "attention_mask"], output_names=["logits"], opset_version=11, dynamic_axes={"input_ids": [0, 1], "attention_mask": [0, 1], "logits": [0, 1]})
--- workflow_scripts/run_python.py
+++ workflow_scripts/run_python.py
@@ -4,9 +4,18 @@ import sys
 from pathlib import Path
 
 python_root = "models/python"
+cache_dir = ".cache"
 cwd_path = Path.cwd()
 errors = 0
 total_models = 0
+ZOO_OPSET_VERSION = 18
+
+def find_base_onnx(root_dir):
+    for root, _, files in os.walk(root_dir):
+        for file in files:
+            if "-base.onnx" in file:
+                return os.path.join(root, file)
+    return None
 
 for root, dirs, files in os.walk(python_root):
     for dir in dirs:
@@ -31,16 +40,31 @@ for root, dirs, files in os.walk(python_root):
             continue
         subprocess.run(["pip", "install", "-r", requirements_path], cwd=cwd_path, stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE)
+        os.remove(model_path)
         subprocess.run(["python", model_python_path], cwd=cwd_path, stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE)
-        os.replace(model_name, model_path)
-        if not os.path.exists(model_path):
+        if not os.path.exists(model_name):
             print(f"Model {model_path} was not created by {model_python_path}.")
             errors += 1
             continue
+        os.replace(model_name, model_path)
         subprocess.run(["git", "diff", "--exit-code", "--", model_path],
                         cwd=cwd_path, stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE)
+        os.remove(model_path)
+        subprocess.run(["benchit", model_python_path, "--cache-dir", cache_dir, "--onnx-opset", ZOO_OPSET_VERSION],
+                        cwd=cwd_path, stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+        cache_model = find_base_onnx(cache_dir, dir)
+        if cache_dir is None:
+            print(f"Model {model_path} was not created by benchit from mlagility.")
+            errors += 1
+            continue
+        os.replace(cache_model, model_path)
+        subprocess.run(["git", "diff", "--exit-code", "--", model_path],
+                        cwd=cwd_path, stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+
         print(f"Successfully checked {model_path}.")
 
 
