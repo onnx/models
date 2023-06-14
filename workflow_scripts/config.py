7af0369af150b846406bc827b894b8589349fbf0
jcwchen <jacky82226@gmail.com>
2023-06-14 14:49:11 -0700

    new config
    Signed-off-by: jcwchen <jacky82226@gmail.com>

--- workflow_scripts/config.py
+++ workflow_scripts/config.py
@@ -1,5 +1,5 @@
 models_info = [
     # (script_path, model_name, model_zoo_path)
-    ("torch_hub/alexnet.py", "alexnet_torch_hub_7df2a577", "alexnet-18/alexnet-18.onnx"),
-    ("torchvision/fasterrcnn_resnet50_fpn_v2.py", "fasterrcnn_resnet50_fpn_v2_torchvision_ec445cac", "faster-rcnn/fasterrcnn_resnet50_fpn_v2.onnx"),
+    ("torch_hub/alexnet.py", "alexnet_torch_hub_7df2a577", "alexnet/alexnet.onnx"),
+    ("torchvision/fasterrcnn_resnet50_fpn_v2.py", "fasterrcnn_resnet50_fpn_v2_torchvision_ec445cac", "fasterrcnn_resnet50_fpn_v2/fasterrcnn_resnet50_fpn_v2.onnx"),
 ]
