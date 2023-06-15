0ef4ef4e3abb0cf8459639893c65ab808e261079
jcwchen <jacky82226@gmail.com>
2023-06-15 15:56:30 -0700

    clean path
    Signed-off-by: jcwchen <jacky82226@gmail.com>

--- workflow_scripts/config.py
+++ workflow_scripts/config.py
@@ -1,6 +1,6 @@
 models_info = [
-    # (script_path, model_name, model_zoo_path)
-    ("torch_hub/alexnet.py", "alexnet_torch_hub_7df2a577", "alexnet/alexnet.onnx"),
-    ("torch_hub/resnet50.py", "resnet50_torch_hub_31acb52e", "resnet/resent50.onnx"),
-    ("torchvision/fasterrcnn_resnet50_fpn_v2.py", "fasterrcnn_resnet50_fpn_v2_torchvision_ec445cac", "fasterrcnn_resnet50_fpn_v2/fasterrcnn_resnet50_fpn_v2.onnx"),
+    # (script_path, model_name, model_zoo_directory)
+    ("torch_hub/alexnet.py", "alexnet_torch_hub_7df2a577", "alexnet"),
+    ("torch_hub/resnet50.py", "resnet50_torch_hub_31acb52e", "resnet50"),
+    ("torchvision/fasterrcnn_resnet50_fpn_v2.py", "fasterrcnn_resnet50_fpn_v2_torchvision_ec445cac", "fasterrcnn_resnet50_fpn_v2"),
 ]
--- workflow_scripts/run_mlagility.py
+++ workflow_scripts/run_mlagility.py
@@ -6,30 +6,31 @@ import subprocess
 import sys
 
 
-base_name = "-op18-base.onnx"
+ZOO_OPSET_VERSION = "18"
+base_name = f"-op{ZOO_OPSET_VERSION}-base.onnx"
 cwd_path = Path.cwd()
 mlagility_root = "mlagility/models"
 mlagility_models_dir = "models/mlagility"
 cache_converted_dir = ".cache"
-ZOO_OPSET_VERSION = "18"
 
 errors = 0
 
-for script_path, model_name, model_zoo_path in config.models_info:
+for script_path, model_name, model_zoo_dir in config.models_info:
     try:
-        print(f"----------------Checking {model_zoo_path}----------------")
-        final_model_path = osp.join(mlagility_models_dir, model_zoo_path.replace(".onnx", "-" + ZOO_OPSET_VERSION + ".onnx"))
-        subprocess.run(["benchit", osp.join(mlagility_root, script_path), "--cache-dir", cache_converted_dir, "--onnx-opset", ZOO_OPSET_VERSION],
+        print(f"----------------Checking {model_zoo_dir}----------------")
+        final_model_path = osp.join(mlagility_models_dir, model_zoo_dir, f"{model_zoo_dir}-{ZOO_OPSET_VERSION}.onnx")
+        subprocess.run(["benchit", osp.join(mlagility_root, script_path), "--cache-dir", cache_converted_dir,
+                        "--onnx-opset", ZOO_OPSET_VERSION, "--export-only"],
                         cwd=cwd_path, stdout=sys.stdout,
                         stderr=sys.stderr)
         shutil.copy(osp.join(cache_converted_dir, model_name, "onnx", model_name + base_name), final_model_path)
         subprocess.run(["git", "diff", "--exit-code", "--", final_model_path],
                         cwd=cwd_path, stdout=sys.stdout,
                         stderr=sys.stderr)
-        print(f"Successfully checked {model_zoo_path}.")
+        print(f"Successfully checked {model_zoo_dir}.")
     except Exception as e:
         errors += 1
-        print(f"Failed to check {model_zoo_path} because of {e}.")
+        print(f"Failed to check {model_zoo_dir} because of {e}.")
 
 if errors > 0:
     print(f"All {len(config.models_info)} model(s) have been checked, but {errors} model(s) failed.")
