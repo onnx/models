9f528a927adc7e6067aa79fe386e9763a51fc780
jcwchen <jacky82226@gmail.com>
2023-07-13 17:41:36 -0700

    only keep
    Signed-off-by: jcwchen <jacky82226@gmail.com>

--- workflow_scripts/mlagility_config.py
+++ workflow_scripts/mlagility_config.py
@@ -22,8 +22,8 @@ models_info = [
     "popular_on_huggingface/openai_clip-vit-large-patch14.py",
     #"popular_on_huggingface/xlm-roberta-base.py", # output nan
     #"popular_on_huggingface/roberta-base.py", # output nan
-    "popular_on_huggingface/distilbert-base-uncased.py",
+    #"popular_on_huggingface/distilbert-base-uncased.py",
     #"popular_on_huggingface/distilroberta-base.py", # output nan
-    "popular_on_huggingface/distilbert-base-multilingual-cased.py",
+    #"popular_on_huggingface/distilbert-base-multilingual-cased.py",
     #"popular_on_huggingface/albert-base-v2", # Status Message: indices element out of data bounds, idx=8 must be within the inclusive range [-2,1]
 ]
