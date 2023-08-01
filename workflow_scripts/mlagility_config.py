e665a09401b18e75fa26f1aef7640f3fd98a84db
jcwchen <jacky82226@gmail.com>
2023-07-31 17:36:11 -0700

    try 15 models
    Signed-off-by: jcwchen <jacky82226@gmail.com>

--- workflow_scripts/mlagility_config.py
+++ workflow_scripts/mlagility_config.py
@@ -15,15 +15,33 @@ models_info = [
     "torch_hub/densenet121.py",
     "torch_hub/inception_v3.py",
     "torch_hub/googlenet.py",
-    #"transformers/bert_generation.py", # non consistent created model from mlagility
-    #"popular_on_huggingface/bert-base-uncased.py",
-    #"popular_on_huggingface/xlm-roberta-large.py",
-    #"popular_on_huggingface/bert-large-uncased.py",
     "popular_on_huggingface/openai_clip-vit-large-patch14.py",
+    "popular_on_huggingface/distilbert-base-uncased.py",
+    "popular_on_huggingface/distilbert-base-multilingual-cased.py",
+    #"transformers/bert_generation.py", # non consistent created model from mlagility
     #"popular_on_huggingface/xlm-roberta-base.py", # output nan
     #"popular_on_huggingface/roberta-base.py", # output nan
-    "popular_on_huggingface/distilbert-base-uncased.py",
     #"popular_on_huggingface/distilroberta-base.py", # output nan
-    "popular_on_huggingface/distilbert-base-multilingual-cased.py",
     #"popular_on_huggingface/albert-base-v2", # Status Message: indices element out of data bounds, idx=8 must be within the inclusive range [-2,1]
+    #"transformers/bert_generation.py", # non consistent created model from mlagility
+    #"popular_on_huggingface/bert-base-uncased.py", # failed to create test data
+    #"popular_on_huggingface/xlm-roberta-large.py", # failed to create test data
+    #"popular_on_huggingface/bert-large-uncased.py", # failed to create test data
+]
+models_info = [
+    "popular_on_huggingface/AI-Growth-Lab_PatentSBERTa.py",
+    "popular_on_huggingface/AmazonScience_qanlu.py",
+    "popular_on_huggingface/BM-K_KoSimCSE-roberta.py",
+    "popular_on_huggingface/Babelscape_wikineural-multilingual-ner.py",
+    "popular_on_huggingface/Bhuvana_t5-base-spellchecker.py",
+    "popular_on_huggingface/CAMeL-Lab_bert-base-arabic-camelbert-ca-pos-egy.py",
+    "popular_on_huggingface/CompVis_ldm-text2im-large-256.py",
+    "popular_on_huggingface/CompVis_stable-diffusion-v1-4.py",
+    "popular_on_huggingface/DMetaSoul_sbert-chinese-general-v2.py",
+    "popular_on_huggingface/Davlan_bert-base-multilingual-cased-masakhaner.py",
+    "popular_on_huggingface/DmitryPogrebnoy_MedRuRobertaLarge.py",
+    "popular_on_huggingface/ElKulako_cryptobert.py",
+    "popular_on_huggingface/Elron_bleurt-base-512.py",
+    "popular_on_huggingface/FinanceInc_finbert_fls.py",
+    "popular_on_huggingface/HooshvareLab_bert-fa-zwnj-base-ner.py",
 ]
