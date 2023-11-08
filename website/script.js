ec4c61d4299c658fc312be9fbb98ca01c7810e30
ramkrishna2910 <ramkrishna2910@gmail.com>
2023-11-08 15:49:47 -0800

    improve website performance

--- website/script.js
+++ website/script.js
@@ -1,6 +1,6 @@
 
 let currentPage = 1;
-const itemsPerPage = 18;
+const itemsPerPage = 36;
 
 const renderCards = function (data) {
   // function renderCards(data) {
@@ -28,86 +28,105 @@ const renderCards = function (data) {
     pageInfo.textContent = `${currentPage}/${totalPages}`;
 };
 
-console.log("!!!!!!!!!!",typeof renderCards)
-
 document.addEventListener('DOMContentLoaded', function () {
   const authorFilterContainer = document.getElementById('author-filters');
   const taskFilterContainer = document.getElementById('task-filters');
+  const opsetFilterContainer = document.getElementById('opset-filters');
 
   let tasksSet = new Set();  // Create a new Set object for tasks
   let filteredData = [];
   let authorsSet = new Set();
+  let opsetSet = new Set();
 
   async function fetchData() {
     const response = await fetch('https://api.github.com/repos/aigdat/onnx-models/git/trees/main?recursive=1');
     const data = await response.json();
     const onnxFiles = data.tree.filter(item => item.path.endsWith('.onnx') && !item.path.includes('archive/'));
     const yamlFiles = data.tree.filter(item => item.path.endsWith('turnkey_stats.yaml'));
-    const modelData = [];
-    for (const file of onnxFiles) {
+  
+    // Create a promise for each YAML file fetch operation
+    const fetchYamlPromises = onnxFiles.map(file => {
       const pathParts = file.path.split('/');
-      const parentDir = pathParts.slice(0, -1).join('/');  // Exclude the ONNX filename to get its directory
-      const parentDirParts = parentDir.split('/');
-      const grandParentDir = parentDirParts.slice(0, -1).join('/');  // Get the parent directory of the ONNX file
-      const yamlFile = yamlFiles.find(yaml => yaml.path === `${grandParentDir}/turnkey_stats.yaml`);
-      let modelTitle = pathParts[pathParts.length - 1].replace('.onnx', ''); // Default to ONNX file name
-      let author = 'False';
+      const parentDir = pathParts.slice(0, -1).join('/');
+      const yamlFile = yamlFiles.find(yaml => yaml.path === `${parentDir}/turnkey_stats.yaml`);
+      if (yamlFile) {
+        return fetch(`https://raw.githubusercontent.com/aigdat/onnx-models/main/${yamlFile.path}`)
+          .then(response => response.text())
+          .then(yamlText => ({ file, yamlText }))
+          .catch(error => {
+            console.error('Error fetching YAML:', error);
+            return { file, yamlText: null };  // Ensure the structure is consistent for error cases
+          });
+      }
+      // If no YAML file is associated, resolve to null
+      return Promise.resolve({ file, yamlText: null });
+    });
+  
+    // Wait for all the YAML fetch operations to complete
+    const results = await Promise.all(fetchYamlPromises);
+  
+    // Process the results to create model data
+    const modelData = results.map(({ file, yamlText }) => {
+      let modelTitle = file.path.split('/').pop().replace('.onnx', ''); // Default to ONNX file name
+      let author = 'Unknown';
       let opset = 'NA';
       let task = 'NA';
-      if (yamlFile) {
-        const yamlResponse = await fetch(`https://raw.githubusercontent.com/aigdat/onnx-models/main/${yamlFile.path}`);
-        const yamlText = await yamlResponse.text();
+  
+      if (yamlText) {
         const yamlLines = yamlText.split('\n');
-        // Variable to keep track if we are under 'onnx_model_information' section
         let insideOnnxModelInfo = false;
+  
         for (const line of yamlLines) {
           if (line.startsWith('onnx_model_information:')) {
             insideOnnxModelInfo = true;
             continue;
           }
-
           if (insideOnnxModelInfo) {
             if (line.startsWith('  opset:')) {
               opset = line.split(':')[1].trim();
             }
-
-            // Reset if we're no longer inside the 'onnx_model_information' section
             if (!line.startsWith('  ')) {
               insideOnnxModelInfo = false;
             }
           }
         }
+  
         const modelNameLine = yamlLines.find(line => line.startsWith('model_name:'));
-        if (modelNameLine) {
-          modelTitle = modelNameLine.split(':')[1].trim(); // Override with model name from YAML
-        }
         const authorLine = yamlLines.find(line => line.startsWith('author:'));
         const opsetLine = yamlLines.find(line => line.startsWith('opset:'));
         const taskLine = yamlLines.find(line => line.startsWith('task:'));
+  
+        if (modelNameLine) {
+          modelTitle = modelNameLine.split(':')[1].trim();
+        }
         if (authorLine) {
           author = authorLine.split(':')[1].trim();
           authorsSet.add(author);
         }
         if (opsetLine) {
           opset = opsetLine.split(':')[1].trim();
+          opsetSet.add(opset);
         }
         if (taskLine) {
           task = taskLine.split(':')[1].trim().toLowerCase()
-            .replace(/_/g, ' ')  // Replace underscores with spaces
+            .replace(/_/g, ' ')
             .split(' ')
             .map(word => word.charAt(0).toUpperCase() + word.slice(1))
             .join(' ');
-          tasksSet.add(task);  // Add the task to tasksSet
+          tasksSet.add(task);
         }
       }
-      modelData.push({
+  
+      return {
         title: modelTitle,
         description: `Task: ${task}`,
         author,
         opset,
         downloadUrl: `https://github.com/aigdat/onnx-models/raw/main/${file.path}`
-      });
-    }
+      };
+    });
+  
+    // Update the filter buttons in the UI
     authorsSet.forEach(author => {
       const authorFilter = document.createElement('div');
       authorFilter.className = 'filter-button';
@@ -115,7 +134,7 @@ document.addEventListener('DOMContentLoaded', function () {
       authorFilter.textContent = author;
       authorFilterContainer.appendChild(authorFilter);
     });
-    // Create filter buttons for tasks
+  
     tasksSet.forEach(task => {
       const taskFilter = document.createElement('div');
       taskFilter.className = 'filter-button';
@@ -123,6 +142,15 @@ document.addEventListener('DOMContentLoaded', function () {
       taskFilter.textContent = task;
       taskFilterContainer.appendChild(taskFilter);
     });
+  
+    opsetSet.forEach(opset => {
+      const opsetFilter = document.createElement('div');
+      opsetFilter.className = 'filter-button';
+      opsetFilter.setAttribute('data-value', opset);
+      opsetFilter.textContent = opset;
+      opsetFilterContainer.appendChild(opsetFilter);
+    });
+  
     return modelData;
   }
 
@@ -132,6 +160,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let activeAuthorFilters = new Set();
     let activeTaskFilters = new Set();
+    let activeOpsetFilters = new Set();
 
     const filterOptions = document.querySelectorAll('.filter-button');
 
@@ -147,20 +176,25 @@ document.addEventListener('DOMContentLoaded', function () {
             activeAuthorFilters.add(filterValue);
           } else if (parentContainer === 'task-filters') {
             activeTaskFilters.add(filterValue);
+          } else if (parentContainer === 'opset-filters') {
+            activeOpsetFilters.add(filterValue);
           }
         } else {
           if (parentContainer === 'author-filters') {
             activeAuthorFilters.delete(filterValue);
           } else if (parentContainer === 'task-filters') {
             activeTaskFilters.delete(filterValue);
-          }
+          } else if (parentContainer === 'opset-filters') {
+            activeOpsetFilters.delete(filterValue);
+          } 
         }
 
         // Apply the filters
         filteredData = data.filter(item => {
           const authorMatches = activeAuthorFilters.size === 0 || activeAuthorFilters.has(item.author);
           const taskMatches = activeTaskFilters.size === 0 || activeTaskFilters.has(item.description.split(': ')[1]);
-          return authorMatches && taskMatches;
+          const opsetMatches = activeOpsetFilters.size === 0 || activeOpsetFilters.has(item.opset);
+          return authorMatches && taskMatches && opsetMatches;
         });
 
         currentPage = 1;
@@ -194,6 +228,5 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-console.log("#################",typeof renderCards)
 module.exports = { fetchData, renderCards };
 // exports = {fetchData, renderCards}
